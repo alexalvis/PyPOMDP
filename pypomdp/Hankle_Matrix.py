4a84729f397017439f51dd2fafef37ae0483d722
Ma <hma2@ufl.edu>
2022-12-02 15:25:47 -0500

    Still cant get Hankel Matrix

--- pypomdp/Hankle_Matrix.py
+++ pypomdp/Hankle_Matrix.py
@@ -7,19 +7,25 @@ Created on Mon Nov 28 00:07:28 2022
 
 from splearn.datasets.base import load_data_sample
 from splearn.spectral import Spectral
-from splearn.spectral import Hankel
+from splearn  import Hankel
+from splearn.automaton import Automaton
 
 def Hankel_Matrix(filename):
     train = load_data_sample(filename)
     
     est = Spectral()
+    est.set_params(lrows = 3, lcolumns = 3, smooth_method = 'trigram', version = 'factor', rank = 8)
     est.fit(train.data)
-    lhankel = Hankel( sample_instance=train.data,
-                     nbL=train.nbL, nbEx=train.nbEx, 
-                     lrows=6, lcolumns=6, version="classic", 
-                     partial=True, sparse=True, mode_quiet=True).lhankel
+    # print(est.automaton.transitions)
+    # print(train.fact)
+    lhankel = Hankel( sample_instance=None, 
+                      lrows=6, lcolumns=6, version="classic", 
+                      partial=True, sparse=True, mode_quiet=True).lhankel
+    return train
     return lhankel
 
 if __name__ == "__main__":
     filename = "Test_store.json"
+    auto = Automaton.SimpleExample()
+    print(auto.transitions)
     lhankel = Hankel_Matrix(filename)   
